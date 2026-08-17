16a7c87fab58d0a88c5deeb2375647d206a2f76c
Kawa Vinicius <kawavinicioss@gmail.com>
2026-08-17 19:14:33 -0300

    feat: adiciona exercicio

---
+++ 01-salary-java/01-salary-java/src/app/Program.java
@@ -0,0 +1,27 @@
+package app;
+
+import entities.Employee;
+import services.SalaryService;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nome: ");
+        String name =sc.nextLine();
+        System.out.print("Salario bruto: ");
+        double grossSalary = sc.nextDouble();
+
+        Employee employee = new Employee(name,grossSalary);
+
+        SalaryService salaryService = new SalaryService();
+
+        double netSalary = salaryService.netSalary(employee);
+        System.out.printf("Salario liquido: %.2f%n", netSalary);
+
+        sc.close();
+    }
+}
